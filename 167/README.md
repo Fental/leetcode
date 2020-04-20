4b43a3e49f3bbfd5b92aabadded0f956738a2e3c
Fental Huang <fengeeker@gmail.com>
2020-04-20 21:26:26 +0800

    done 167

---
+++ 167/README.md
@@ -0,0 +1,9 @@
+# 167. 两数之和 II - 输入有序数组
+
+## 题目链接
+
+https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+
+## 思路
+
+双指针
---
+++ 167/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let i = 0;
+    let j = numbers.length - 1;
+
+    while (j > i) {
+        let sum = numbers[i] + numbers[j];
+        if (sum === target) {
+            return [i + 1, j + 1];
+        }
+        if (sum > target) {
+            j--;
+        } else {
+            i++;
+        }
+    }
+
+    return [];
+};
