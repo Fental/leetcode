ac3f13dfda4c6fe781e4bb4bd26eff0426164bc5
Fental Huang <fengeeker@gmail.com>
2020-04-30 00:10:21 +0800

    done 226

---
+++ 226/README.md
@@ -0,0 +1,14 @@
+# 226. 翻转二叉树
+
+## 题目链接
+
+https://leetcode-cn.com/problems/invert-binary-tree/
+
+## 思路
+
+递归
+
+
+## 其他思路
+
+迭代 TODO
---
+++ 226/index.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (root == null) {
+        return null;
+    }
+
+    let tmp = root.right;
+    root.right = invertTree(root.left);
+    root.left = invertTree(tmp);
+    return root;
+};
